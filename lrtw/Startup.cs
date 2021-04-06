0f0623e04b91a0907b2d5e95b0df8a201d9ca4cb
cowtrix <seandgfinnegan@gmail.com>
2021-04-06 05:12:23 -0700

    Added Https redirection

--- lrtw/Startup.cs
+++ lrtw/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
 
 namespace lrtw
 {
@@ -25,6 +27,11 @@ namespace lrtw
 		{
 			services.AddControllersWithViews();
 			services.AddResponseCompression();
+			services.AddHttpsRedirection(options =>
+			{
+				options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+				options.HttpsPort = 443;
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +47,7 @@ namespace lrtw
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-			//app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
